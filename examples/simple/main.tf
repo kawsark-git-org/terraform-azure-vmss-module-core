02d274d45bdded4221fc33f5b17d1e089befbfda
kawsark <kawsar@hashicorp.com>
2018-10-04 12:25:46 -0400

    Fixed module url

--- examples/simple/main.tf
+++ examples/simple/main.tf
@@ -17,7 +17,7 @@ provider "azurerm" {}
 
 #Instantiate core module:
 module "core" {
-  source  = "app.terraform.io/CI-demo-org/vmss-module-app/core"
+  source  = "app.terraform.io/CI-demo-org/vmss-module-core/azure"
   version = "1.2"
   location = "${var.location}"
   core_resource_group_name = "${var.core_resource_group_name}"
